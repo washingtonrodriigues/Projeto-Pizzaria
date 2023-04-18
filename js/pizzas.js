3bb4ac84c6b70860352539850e96e64e37782622
Washington Rodrigues <washington_sr_@hotmail.com>
2023-04-18 14:51:26 -0300

    terminei a estilização para desktop e fiz algumas funcionalidades de carrinho

---
+++ js/pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/cardapio/mussarela.png', price:25.00, sizes:['8', '10', '12'], description:'Molho de tomate, camada dupla de mussarela e orégano'},
+    {id:2, name:'Calabresa', img:'images/cardapio/calabresa.png', price:26.00, sizes:['8', '10', '12'], description:'Molho de tomate, mussarela, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/cardapio/4queijos.png', price:28.00, sizes:['8', '10', '12'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
+    {id:4, name:'Brasileira', img:'images/cardapio/brasileira.png', price:30.00, sizes:['8', '10', '12'], description:'Molho de tomate, mussarela, calabresa picada, palmito, champignon, azeitonas e orégano'},
+    {id:5, name:'Portuguesa', img:'images/cardapio/portuguesa.png', price:29.00, sizes:['8', '10', '12'], description:'Molho de tomate, mussarela, presunto, ovos, cebolas, pimentão, azeitona  e orégano'},
+    {id:6, name:'Moda da Casa', img:'images/cardapio/modadacasa.png', price:35.00, sizes:['8', '10', '12'], description:'Molho de tomate, mussarela, carne de sol, tomates em cubos, coentro, cebola, azeitona, catupiry e orégano'},
+    {id:7, name:'Frango com Catupiry', img:'images/cardapio/frango.png', price:32.00, sizes:['8', '10', '12'], description:'Frango desfiado, catupiry, tomate e orégano'},
+    {id:8, name:'Marguerita', img:'images/cardapio/marguerita.png', price:35.00, sizes:['8', '10', '12'], description:'Mussarela, parmesão ralado, rodelas de tomate e folhas de manjericão'}
+];
---
+++ js/script.js
@@ -0,0 +1,31 @@
+pizzaJson.map((item, index) => {
+    //fazendo um clone do elemento pizza-item que está dentro da div models no html para a pizza-area
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+    document.querySelector('.pizza-area').append(pizzaItem)
+
+    //preenchendo os dados do item (pizza)
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+
+    //pizza clicada
+    pizzaItem.querySelector('.pizza-item a').addEventListener('click', (ev) => {
+        ev.preventDefault()
+        //abrir janela de adição
+        document.querySelector('.pizzaWindowArea').style.display = 'flex'
+        //preencher os dados da janela
+        document.querySelector('.pizzaBig img').src = item.img
+        document.querySelector('.pizzaInfo h1').innerHTML = item.name
+        document.querySelector('.pizzaInfo--desc').innerHTML = item.description
+        document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+
+    })
+
+    document.querySelector('.pizzaInfo--cancelButton').addEventListener('click', () => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
+    document.querySelector('.pizzaWindowArea').addEventListener('click', () => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none'
+    })
+})
